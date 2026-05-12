54703e1375f351e007d35861b96a413625a7781c
Preyan Bhowmick <preyan1997@gmail.com>
2026-05-13 00:03:32 +0530

    feat: enhance blog functionality with new post, image component, and style updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://preyan.github.io',
   output: 'static',
-
+  integrations: [mdx(), sitemap()],
+  image: {
+    domains: ['images.unsplash.com'],
+    remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [mdx(), sitemap()],
 });
